f1ec17f067bb7c28dc7341f812078aa50390117a
Mahesh Dhakare <Dhakarem@gmail.com>
2019-01-12 19:06:29 +0530

    Updated Test cases with respect to separate class for reusable methods

--- app/src/androidTest/java/com/mytaxi_MobileAppAutomationTest/HomeTest_TestCases.java
+++ app/src/androidTest/java/com/mytaxi_MobileAppAutomationTest/HomeTest_TestCases.java
@@ -14,15 +14,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-/*import static android.support.test.espresso.action.ViewActions.click;*/
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-/*import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;*/
-/*import static org.hamcrest.Matchers.allOf;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;*/
+import static org.hamcrest.Matchers.is;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
